eb1a6bbc4c5d40cd45cd24fb66f4880084db3435
whalecore <komnatakita@gmail.com>
2020-10-18 11:49:38 +0300

    fixed audio method

--- bot.py
+++ bot.py
@@ -1,101 +1,9 @@
-# import random
-# from string import ascii_lowercase
-# from aiogram import Bot, Dispatcher, executor, types
-# # from aiogram.dispatcher import FSMContext
-# # from aiogram.dispatcher.filters.state import State, StatesGroup
-# import logging
-# import time
-# import requests
-# import aiogram.utils.markdown as md
-# from aiogram.dispatcher import FSMContext
-
-
-# from aiogram.types.message import ContentTypes
-
-# from functions import hi_pitch, low_pitch, remove_all_oggs
-# from parser import search_wiki
-
-# BOT_TOKEN = '1252484349:AAGC4t5-PzwZq-0Q56mOxfduGfcZamDSWDo'
-
-# bot = Bot(token=BOT_TOKEN)
-# dp = Dispatcher(bot)
-
-# logging.basicConfig(level=logging.DEBUG)
-
-# keyboard = types.ReplyKeyboardMarkup(
-#     resize_keyboard=True, one_time_keyboard=False)
-# person_btn = types.KeyboardButton('Получить инфо из Википедии')
-# voice = types.KeyboardButton('Преобразовать голос')
-# face = types.KeyboardButton('Распознавание лиц (в разработке)')
-# keyboard.add(person_btn, voice, face)
-
-
-# # class MakeWikiRequest(StatesGroup):
-# #     waiting_for_request = State()
-# #     waiting_for_audio = State()
-# #     inactive = State()
-
-
-# @dp.message_handler(commands=['start'])
-# async def start(message: types.Message):
-#     await message.answer('Привет! Я чат-бот Наполеон. Клавиатура для использования доступна ниже: \
-#     Мои функции: \n\n \
-#     1. Поиск по Википедии \n \
-#     2. Изменение тембра голоса \n \
-#     3. Распознавание лица по фото', reply_markup=keyboard)
-
-
-# @dp.message_handler(text=['Получить инфо из Википедии'], state="*")
-# async def wiki(message: types.Message):
-#     await bot.send_message(message.chat.id, "Напишите, что мне поискать")
-#     # await MakeWikiRequest.waiting_for_request.set()
-
-#     @dp.message_handler(content_types=ContentTypes.TEXT)
-#     async def send_wiki(message: types.Message):
-#         # await state.update_data(message=message.text)
-#         links = search_wiki(message.text)
-#         counter = 0
-#         for k, v in links.items():
-#             if counter == 3:
-#                 # await state.finish()
-#                 break
-#             else:
-#                 formatted_string = '<strong>' + k + '</strong>' + '\n\n' + v
-#                 await bot.send_message(message.chat.id, formatted_string, parse_mode='HTML')
-#                 counter += 1
-#                 time.sleep(1)
-
-
-# @dp.message_handler(text=['Преобразовать голос'])
-# async def voice_file(message: types.Audio):
-#     # voice_file = message.file_id
-#     keyboard = types.ReplyKeyboardMarkup(
-#         resize_keyboard=True, one_time_keyboard=False)
-#     hi = types.KeyboardButton('Высокий тембр')
-#     low = types.KeyboardButton('Низкий тембр')
-#     keyboard.add(hi, low)
-#     await bot.send_message(message.chat.id, 'Выберите тембр и запишите аудио', reply_markup=keyboard)
-
-#     @dp.message_handler(text=['Высокий тембр'])
-#     async def high(message: types.Message.voice):
-#         voice_file = message.file_id
-#         url = 'https://api.telegram.org/file/bot{}/{}'.format(
-#             BOT_TOKEN, voice_file)
-#         r = requests.get(url)
-#         random_filename = ''.join(random.choice(
-#             ascii_lowercase) for i in range(8))
-#         with open(random_filename, 'wb') as audiofile:
-#             audiofile.write(r.content)
-#         hi = hi_pitch(random_filename)
-#         await bot.send_file(message.chat.id, voice=hi)
-
-# executor.start_polling(dp, skip_updates=True)
-
+import time
 import logging
 from aiogram.dispatcher import storage
 from aiogram.types import message
 from aiogram.types.message import ContentTypes
-from aiogram.types.reply_keyboard import ReplyKeyboardMarkup
+from aiogram.types.reply_keyboard import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
@@ -103,18 +11,22 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ParseMode
+from aiogram.types import ParseMode, InputMediaAudio
 from aiogram.utils import executor
 from aiogram.utils.markdown import text
 
 from parser import search_wiki
+from aiohttp.helpers import current_task
 
 import requests
-from functions import hi_pitch, low_pitch
+from functions import hi_pitch, low_pitch, low_pitch_new, get_hi_pitch
+from io import BufferedWriter
+
+import random, string
 
 logging.basicConfig(level=logging.DEBUG)
 
-BOT_TOKEN = '1102746633:AAH2i3PRyzXa4AzKNzzoQgM8P20gLmXK4e8'
+BOT_TOKEN = '1252484349:AAGC4t5-PzwZq-0Q56mOxfduGfcZamDSWDo'
 
 bot = Bot(token=BOT_TOKEN)
 
@@ -127,6 +39,8 @@ dp = Dispatcher(bot, storage=storage)
 class Form(StatesGroup):
     wiki = State()
     voice = State()
+    voice_hi = State()
+    voice_low = State()
     image = State()
 
 
@@ -184,7 +98,7 @@ async def voice_pitch(message: types.Message, state: FSMContext):
     await Form.voice.set()
     keyboard = ReplyKeyboardMarkup(
         resize_keyboard=True, one_time_keyboard=False)
-    buttons = ['Высокий', 'Низкий']
+    buttons = ['Высокий', 'Низкий', 'Отмена']
     for button in buttons:
         keyboard.add(button)
     await bot.send_message(
@@ -194,25 +108,72 @@ async def voice_pitch(message: types.Message, state: FSMContext):
     )
 
 
+@dp.message_handler(state='*', text=['Отмена'])
+async def cancel(message: types.Message, state: FSMContext):
+    keyboard = ReplyKeyboardMarkup()
+    btns = ["Вики", "Тембр голоса", "Фото"]
+    for btn in btns:
+        keyboard.add(btn)
+    await state.finish()
+    await bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBdwZfioz_Q2I_fVq77fJlMezQWO7IYAAC2gcAAmMr4gmP2IPUry3pGhsE')
+    await bot.send_message(message.chat.id, 'Ваше действие отменено, mon ami.', reply_markup=keyboard)
+
+
 @dp.message_handler(state=Form.voice, text=['Высокий'])
 async def get_voice(message: types.Message, state: FSMContext):
+    await Form.voice_hi.set()
+    await bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBdzZfiq_jEecS7wNcCWhIMbmplm1kSQAC4QcAAmMr4gnzHOpFutpxDxsE')
     await bot.send_message(
         message.chat.id,
-        "А теперь отправьте мне голосовое сообщение."
+        "Прошу вас записать мне голосовое сообщение, mon ami!"
     )
 
 
-@dp.message_handler(state=Form.voice, content_types=ContentTypes.VOICE)
+@dp.message_handler(state=Form.voice_hi, content_types=ContentTypes.VOICE)
 async def high(message: types.Message, state: FSMContext):
     await bot.send_message(
         message.chat.id,
         'Получите, распишитесь!'
     )
-    await state.finish()
+    await Form.voice.set()
 
     file_info = await bot.get_file(message.voice.file_id)
-    downloaded_file = await bot.download_file(file_info.file_path, 'test.ogg')
-    hp_file = await hi_pitch(downloaded_file)
-    await bot.send_audio(message.chat.id, hp_file)
+    random_name = ''.join(random.choice(string.ascii_lowercase) for i in range(8)) + '.ogg'
+    await bot.download_file(file_info.file_path, random_name)
+    hp_file = hi_pitch(random_name)
+    await bot.send_audio(message.chat.id, audio=open(hp_file, 'rb'))
+
+
+@dp.message_handler(state=Form.voice, text=['Низкий'])
+async def get_voice(message: types.Message, state: FSMContext):
+    await Form.voice_low.set()
+    await bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBdzZfiq_jEecS7wNcCWhIMbmplm1kSQAC4QcAAmMr4gnzHOpFutpxDxsE')
+    await bot.send_message(
+        message.chat.id,
+        "Прошу вас записать мне голосовое сообщение, mon ami!"
+    )
+
+@dp.message_handler(state=Form.voice_low, content_types=ContentTypes.VOICE)
+async def high(message: types.Message, state: FSMContext):
+    await bot.send_message(
+        message.chat.id,
+        'Получите, распишитесь!'
+    )
+    await Form.voice.set()
+
+    file_info = await bot.get_file(message.voice.file_id)
+    random_name = ''.join(random.choice(string.ascii_lowercase) for i in range(8)) + '.ogg'
+    await bot.download_file(file_info.file_path, random_name)
+    hp_file = low_pitch(random_name)
+    await bot.send_audio(message.chat.id, audio=open(hp_file, 'rb'))
+
+@dp.message_handler(state="*", text=['Фото'])
+async def photo(message: types.Message):
+    await bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBdrxfigFLzdXbHhCGk00WOF_AdKQlqwAC3QcAAmMr4glNJX_q7yBeaxsE')
+    await bot.send_message(
+        message.chat.id,
+        "Увы, monsiuer/madam, данная функция все еще в разработке."
+    )
+
 
 executor.start_polling(dp, skip_updates=True)
--- functions.py
+++ functions.py
@@ -1,10 +1,15 @@
 from pydub import AudioSegment
+from google_images_search import GoogleImagesSearch
+
 import random
 import string
 import os
 import glob
+import io
+
 
 octaves = 0.5
+gis = GoogleImagesSearch('AIzaSyCgtnY9D1Ai-OOa5ZJNiCoP5F1EiSq_4pU', '296ed5193c8ec3c4b')
 
 
 def hi_pitch(sound):
@@ -19,8 +24,8 @@ def hi_pitch(sound):
     return random_filename + '.ogg'
 
 
-def low_pitch(sound):
-    sound = AudioSegment.from_file(sound, format='ogg')
+def low_pitch(sound1):
+    sound = AudioSegment.from_file(sound1, format='ogg')
     new_sample_rate = int(sound.frame_rate * (1.5 * octaves))
     hipitch_sound = sound._spawn(sound.raw_data, overrides={
         'frame_rate': new_sample_rate})
@@ -28,7 +33,8 @@ def low_pitch(sound):
     random_filename = ''.join(random.choice(
         string.ascii_lowercase) for i in range(8))
     hipitch_sound.export(random_filename + '.ogg', format='ogg')
-    return random_filename + '.ogg'
+    final_name = random_filename + '.ogg'
+    return final_name
 
 
 def remove_all_oggs():
@@ -36,3 +42,31 @@ def remove_all_oggs():
     for f in BASE_DIR:
         if f.endswith('.ogg'):
             os.remove(f)
+
+def google_photo():
+    _search_params = {
+    'q': 'щенок',
+    'num': 10,
+    'safe': 'medium',
+    'fileType': 'jpg',
+    'imgType': 'photo',
+    'imgSize': 'MEDIUM',
+    'imgDominantColor': '',
+    'rights': 'cc_publicdomain'
+    }
+    gis.search(search_params=_search_params)
+    for image in gis.results():
+        image.download('/home/whalecore/PyProjects/miha')
+        image.resize(500, 500)
+
+def low_pitch_new(zvuk):
+    sound = AudioSegment.from_file(zvuk, format='ogg')
+    print(sound.frame_rate)
+    octaves = -0.5
+    new_sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+    lowpitch_sound = sound._spawn(sound.raw_data, overrides={'frame_rate': new_sample_rate})
+    lowpitch_sound.export('testing.ogg', format='ogg')
+
+def get_hi_pitch(sound):
+    a = hi_pitch(sound)
+    return a
